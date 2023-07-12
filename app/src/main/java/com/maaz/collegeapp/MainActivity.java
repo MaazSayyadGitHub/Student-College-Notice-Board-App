f0a9d09a3c2b5143f726c7f9fc2dd0050a1573b6
Maaz <maazsayyad2206@gmail.com>
2023-07-12 18:20:12 +0530

    Themes Feature Added In MainActivity.java Class.

--- app/src/main/java/com/maaz/collegeapp/MainActivity.java
+++ app/src/main/java/com/maaz/collegeapp/MainActivity.java
@@ -2,19 +2,25 @@ package com.maaz.collegeapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.navigation.NavigationView;
 import com.maaz.collegeapp.EBooks.EBookActivity;
 
@@ -40,6 +46,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private ActionBarDrawerToggle toggle;
     private NavigationView navigationView;
 
+    // SharedPreference For Color Themes
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+
+    private int checkedItem;
+    private String selected;
+    private final String CHECKED_ITEM = "checked_item"; // to access SP.
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -62,6 +76,24 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // bottom navigation
         navController = Navigation.findNavController(this, R.id.frame_layout);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
+
+        // initialize SP.
+        sharedPreferences = this.getSharedPreferences("Themes", MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+
+        // Setting Theme Here
+        switch (getCheckedItem()){
+            case 0:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                break;
+            case 1:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                break;
+            case 2:
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                break;
+        }
+
     }
 
     // this is for toggle click... toggle is three line on navigation Drawer.
@@ -98,8 +130,74 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.navigation_share:
                 Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show();
                 break;
+            case R.id.navigation_color:
+                showDialog();
+                break;
         }
         return true;
+    }
+
+    private void showDialog() {
+
+        String[] themes = this.getResources().getStringArray(R.array.Theme); // getting types of theme array from string.
+
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(this);
+        builder.setTitle("Themes");
+        builder.setSingleChoiceItems(R.array.Theme, getCheckedItem(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // i is the value of what we have checked/selected from dialog.
+                // put that into.
+                selected = themes[i];
+                checkedItem = i;
+            }
+        });
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (selected == null) {
+                    // we will put default on it. if not selected
+                    selected = themes[i];
+                    checkedItem = i;
+                }
+
+                // Setting Theme Here
+                switch (selected){
+                    case "System Default":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+                        break;
+                    case "Dark":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                        break;
+                    case "Light":
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                        break;
+                }
+                setCheckedItem(checkedItem); // set this in SharedPreference
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        // display dialog.
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+    }
+
+    private int getCheckedItem(){
+        return sharedPreferences.getInt(CHECKED_ITEM, 0);
+    }
+
+    private void setCheckedItem(int i){
+        editor.putInt(CHECKED_ITEM, i);
+        editor.apply();
     }
 
     @Override
